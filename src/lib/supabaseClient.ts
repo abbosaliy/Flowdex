8222f4dd22546eece7bbcf56f2f61b5ebb400f21
abbosaliy <abbosbekanvarjonov8@gmail.com>
2026-01-25 00:13:52 +0100

    "force rebuild with env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/database.types';
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/database.types";
 
-const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log("supabase url:", import.meta.env.VITE_SUPABASE_URL);
 
 export default supabase;
